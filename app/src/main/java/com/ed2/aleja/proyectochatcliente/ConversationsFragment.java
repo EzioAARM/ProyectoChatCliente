cfe194a0f5fb072de404ece7ecbdc0a9e9c25e66
EzioAARM <alejandrom9712@gmail.com>
2018-11-21 17:13:09 -0600

    Actualizaciones de diseño

--- app/src/main/java/com/ed2/aleja/proyectochatcliente/ConversationsFragment.java
+++ app/src/main/java/com/ed2/aleja/proyectochatcliente/ConversationsFragment.java
@@ -1,34 +1,24 @@
 package com.ed2.aleja.proyectochatcliente;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.Window;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.ed2.aleja.objetos.Conversaciones;
-import com.ed2.aleja.objetos.Usuario;
-import com.ed2.aleja.utilidades.IHttpRequests;
 import com.ed2.aleja.utilidades.JwtUtility;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
-import java.io.IOException;
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ConversationsFragment extends Fragment {
 
@@ -39,17 +29,46 @@ public class ConversationsFragment extends Fragment {
 
     String emisor = "", _id = "", nuevos = "", imagenURL = "";
     ArrayList<ConversationListViewItem> conversaciones = null;
+
+    FloatingActionButton nuevoChat;
+    FloatingActionButton nuevoGrupo;
+    boolean isFABOpen = false;
     int i;
     JsonArray arrayConversaciones;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.conversations_fragment, container, false);
 
+        FloatingActionButton nuevaConversacion = (FloatingActionButton) rootView.findViewById(R.id.nuevo_conversacion);
+        nuevoChat = (FloatingActionButton) rootView.findViewById(R.id.crear_chat);
+        nuevoGrupo = (FloatingActionButton) rootView.findViewById(R.id.crear_grupo);
+        nuevaConversacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
         JwtUtility jwtUtility = new JwtUtility();
         UsuarioActual = jwtUtility.retornarUsername(rootView.getContext());
         tokenActual = jwtUtility.retornarToken(rootView.getContext());
 
         return rootView;
+    }
+
+    private void showFABMenu(){
+        isFABOpen=true;
+        nuevoChat.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        nuevoGrupo.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        nuevoChat.animate().translationY(0);
+        nuevoGrupo.animate().translationY(0);
     }
 
 }
--- app/src/main/java/com/ed2/aleja/proyectochatcliente/ProfileFragment.java
+++ app/src/main/java/com/ed2/aleja/proyectochatcliente/ProfileFragment.java
@@ -14,6 +14,9 @@ public class ProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        
+
         return rootView;
     }
 
